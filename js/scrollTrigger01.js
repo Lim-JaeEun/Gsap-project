9019b930a498be671f9b0c3134a05bd9721196b1
Lim-JaeEun <jaeun09@naver.com>
2023-09-17 02:53:25 +0900

    0917 commit

--- js/scrollTrigger01.js
+++ js/scrollTrigger01.js
@@ -1,15 +1,59 @@
-let tlMain = gsap.timeline({
-    scrollTrigger:{
-        trigger:'.section-height',
-        start:'top top',
-        end:'98% bottom',
-        scrub:3,
-        
+
+
+
+// Horizontal scroll
+let tlMain = gsap
+  .timeline({
+    scrollTrigger: {
+      trigger: ".section-height",
+      start: "top top",
+      end: "98% bottom",
+      scrub: 1,
+      delay:3
     }
-}).to(".track",{
-    xPercent:-100,
-    ease:'none'
+  })
+  .to(".track", {
+    xPercent: -100,
+    ease: "none"
+  })
+  
+  
+  tlMain.to('.hero',{
+    y:'-30vh',
+    duration:3,
+    ease: "power1.out",
+    scrollTrigger:{
+        trigger:'.is-hero',
+        scrub:2,
+        //containerAnimation: tlMain,
+        start:'top top',
+        end:'bottom 50%',
+        //markers:true
+    }
 })
+  
+
+
+/*
+tlMain.to('.is-hero',{
+    yPercent: -70,
+    // duration: 1.8,
+    ease: "power1.in"
+})
+gsap.timeline({
+    scrollTrigger:{
+        trigger:'.hero',
+        containerAnimation: tlMain,
+        start:"left left",
+        end:"right left",
+        scrub:true,
+        markers: true
+    }
+}).to('.is-hero',{
+    yPercent: -70,
+    //duration:1.8
+})*/
+
 
 gsap.timeline({
     scrollTrigger:{
@@ -17,13 +61,12 @@ gsap.timeline({
         containerAnimation: tlMain,
         start:"left left",
         end:"right left",
-        scrub:3,
-       // markers:true
+        scrub:true,
     }
 }).to('.hero-panel_img',{
    // scale:1,
     width:'75%',
-    duration:5
+    //duration:5
 })
 
 gsap.timeline({
@@ -31,18 +74,16 @@ gsap.timeline({
         trigger:'.note-panel',
         containerAnimation: tlMain,
         start:'left right',
-        end:'left 50%',
+        end:'left left',
         scrub:true,
         
     }
 }).from('.note-panel_contain',{
     rotate: 45
-},0)
+},'<')
 .to('.note-panel_contain',{
     xPercent: -150,
-    
-
-},0)
+},'<')
 
 gsap.timeline({
     scrollTrigger:{
@@ -55,20 +96,20 @@ gsap.timeline({
     }
 }).to(".thanks-panel",{
     xPercent:100, ease:'none'
-}).to('.thanks-panel_photo',{
-    scale:1
-},0).fromTo(
+}).fromTo(
     ".thanks-panel_contain.is-2",
     {
       clipPath: "polygon(100% 0%, 100% 0%, 100% 100%, 100% 100%)"
     },
-    { clipPath: "polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)", ease: "none" },
-    0
-  );
+    { clipPath: "polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)", ease: "none" },0)
+    .to('.thanks-panel_photo',{
+        scale:1
+    },0)
 
+
+    
 gsap.timeline({
     scrollTrigger:{
-        markers:true,
         scrub:true,
         containerAnimation: tlMain,
         trigger:'.stagger-panel',
@@ -80,3 +121,21 @@ gsap.timeline({
 }).to('.stagger-panel_img',{
     scale: 0.5, stagger: { each: 0.05 }
 })  
+
+
+gsap.timeline({
+    scrollTrigger:{
+        scrub:true,
+        containerAnimation: tlMain,
+        trigger:'.wide-panel',
+        start:'left 50%',
+        end:'right 40%',
+        //markers: true
+    }
+}).to('.wide-panel_img',{
+    scale:.7
+}).to('.wide-panel_img',{
+    yPercent:-30
+})
+
+
