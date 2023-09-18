405ce9b3fc7755b2e7df2594ec6621f333399e79
Lim-JaeEun <jaeun09@naver.com>
2023-09-18 17:40:19 +0900

    0918 commit

--- js/scrollTrigger01.js
+++ js/scrollTrigger01.js
@@ -1,6 +1,3 @@
-
-
-
 // Horizontal scroll
 let tlMain = gsap
   .timeline({
@@ -8,134 +5,79 @@ let tlMain = gsap
       trigger: ".section-height",
       start: "top top",
       end: "98% bottom",
-      scrub: 1,
-      delay:3
+      scrub: 1
     }
   })
+  .to('.is-hero',{
+    yPercent:-50,
+    ease:"power.in"
+  },0)
   .to(".track", {
     xPercent: -100,
-    ease: "none"
+    ease: "none",
+   // delay:.5
+  },0.1);
+
+// hero photo
+gsap
+  .timeline({
+    scrollTrigger: {
+      trigger: ".hero-panel",
+      containerAnimation: tlMain,
+      start: "left left",
+      end: "right left",
+      scrub: true
+    }
   })
-  
-  
-  tlMain.to('.hero',{
-    y:'-30vh',
-    duration:3,
-    ease: "power1.out",
-    scrollTrigger:{
-        trigger:'.is-hero',
-        scrub:2,
-        //containerAnimation: tlMain,
-        start:'top top',
-        end:'bottom 50%',
-        //markers:true
+  .from(".hero-panel_img", {duration:2, scale: 1.6 },1);
+
+// note
+gsap
+  .timeline({
+    scrollTrigger: {
+      trigger: ".note-panel",
+      containerAnimation: tlMain,
+      start: "left right",
+      end: "left left",
+      scrub: true
     }
-})
-  
+  })
+  .from(".note-panel_contain", { rotate: 45, scale: 0.3},1)
+  .to(".note-panel_contain",{ xPercent:-150 },1)
 
-
-/*
-tlMain.to('.is-hero',{
-    yPercent: -70,
-    // duration: 1.8,
-    ease: "power1.in"
-})
-gsap.timeline({
-    scrollTrigger:{
-        trigger:'.hero',
-        containerAnimation: tlMain,
-        start:"left left",
-        end:"right left",
-        scrub:true,
-        markers: true
+// thanks
+gsap
+  .timeline({
+    scrollTrigger: {
+      trigger: ".thanks-panel_wrap",
+      containerAnimation: tlMain,
+      start: "left left",
+      end: "right right",
+      scrub: true,
+      markers:true
     }
-}).to('.is-hero',{
-    yPercent: -70,
-    //duration:1.8
-})*/
-
-
-gsap.timeline({
-    scrollTrigger:{
-        trigger:'.hero-panel',
-        containerAnimation: tlMain,
-        start:"left left",
-        end:"right left",
-        scrub:true,
-    }
-}).to('.hero-panel_img',{
-   // scale:1,
-    width:'75%',
-    //duration:5
-})
-
-gsap.timeline({
-    scrollTrigger:{
-        trigger:'.note-panel',
-        containerAnimation: tlMain,
-        start:'left right',
-        end:'left left',
-        scrub:true,
-        
-    }
-}).from('.note-panel_contain',{
-    rotate: 45
-},'<')
-.to('.note-panel_contain',{
-    xPercent: -150,
-},'<')
-
-gsap.timeline({
-    scrollTrigger:{
-        trigger:'.thanks-panel_wrap',
-        containerAnimation: tlMain,
-        start:'left left',
-        end:'right right',
-        scrub:true,
-        
-    }
-}).to(".thanks-panel",{
-    xPercent:100, ease:'none'
-}).fromTo(
+  })
+  .to(".thanks-panel", { xPercent: 100, ease: "none", duration:2 })
+  .to(".thanks-panel_photo", { scale: 1,duration:2 }, 0)
+  .fromTo(
     ".thanks-panel_contain.is-2",
     {
       clipPath: "polygon(100% 0%, 100% 0%, 100% 100%, 100% 100%)"
     },
-    { clipPath: "polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)", ease: "none" },0)
-    .to('.thanks-panel_photo',{
-        scale:1
-    },0)
+    { clipPath: "polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)", ease: "none",duration:2 },
+    0
+  );
 
-
-    
-gsap.timeline({
-    scrollTrigger:{
-        scrub:true,
-        containerAnimation: tlMain,
-        trigger:'.stagger-panel',
-        start:'left right',
-        end:'right left'
+// stagger photos
+gsap
+  .timeline({
+    scrollTrigger: {
+      trigger: ".stagger-panel",
+      containerAnimation: tlMain,
+      start: "left right",
+      end: "right left",
+      scrub: true
     }
-}).from('.stagger-panel_img',{
-   x:"100vw", stagger: { each: 0.05 } 
-}).to('.stagger-panel_img',{
-    scale: 0.5, stagger: { each: 0.05 }
-})  
-
-
-gsap.timeline({
-    scrollTrigger:{
-        scrub:true,
-        containerAnimation: tlMain,
-        trigger:'.wide-panel',
-        start:'left 50%',
-        end:'right 40%',
-        //markers: true
-    }
-}).to('.wide-panel_img',{
-    scale:.7
-}).to('.wide-panel_img',{
-    yPercent:-30
-})
-
-
+  })
+  .from(".stagger-panel_img", { x: "100vw", stagger: { each: 0.05 } })
+  .to(".stagger-panel_img", { scale: 0.5, stagger: { each: 0.05 } });
